f6ede433cc0649657cb2eaf7bdedcec222adb42a
杨晓晨 <xiaochenyung@gmail.com>
2017-03-02 15:00:01 +0800

    获取实例变量

--- MJExtensionExample/ViewController.m
+++ MJExtensionExample/ViewController.m
@@ -7,6 +7,18 @@
 //
 
 #import "ViewController.h"
+#import <objc/runtime.h>
+
+@interface Person : NSObject
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) NSArray *friends;
+@property (assign, nonatomic) NSInteger age;
+@property (copy, nonatomic) NSString *name;
+@end
+
+@implementation Person
+
+@end
 
 @interface ViewController ()
 
@@ -16,6 +28,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    unsigned int outCount = 0;
+    Ivar *ivars = class_copyIvarList([Person class], &outCount);
+    
+    // 遍历所有成员变量
+    for (int i = 0; i < outCount; i++) {
+        // 取出i位置对应的成员变量
+        Ivar ivar = ivars[i];
+        const char *name = ivar_getName(ivar);
+        const char *type = ivar_getTypeEncoding(ivar);
+        NSLog(@"成员变量名：%s 成员变量类型：%s",name,type);
+    }
+    // 注意释放内存！
+    free(ivars);
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
